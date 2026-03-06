4125072ad55eda9ecbd826ddb8508a0dad343546
saymonalves <saymonpadilhaalves10@gmail.com>
2026-03-06 11:40:15 -0300

    operadores de atribuição e relacionais

---
+++ operadores/desafios_relacionais.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 11;
+
+// imprimir no console 5 numeros verdadeiros(true)
+
+console.log(a < b);
+console.log(a <= b);
+console.log(b > a);
+console.log(b >= a);
+console.log(a === a);
+
+// imprimir no console 5 numeros falsos(false)
+
+console.log(a > b);
+console.log(a >= b);
+console.log(b < a);
+console.log(b <= a);
+console.log(a == b);
