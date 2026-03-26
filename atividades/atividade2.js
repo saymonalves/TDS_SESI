5d7823a489b4270c635cf5986b3422d55a9b34ad
saymonalves <saymonpadilhaalves10@gmail.com>
2026-03-26 11:50:36 -0300

    atividade 2

---
+++ atividades/atividade2.js
@@ -0,0 +1,5 @@
+var idadeDoCachorro = 6;
+let resultado;
+
+resultado = idadeDoCachorro * 7;
+console.log(`se a idade de um humano for ${idadeDoCachorro}, sua idade e equivalente ${resultado} anos cachorrenses,` )
