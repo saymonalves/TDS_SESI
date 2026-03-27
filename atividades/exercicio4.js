32552fc74dfce2cc0f05d8bb870749455cfaeb7a
saymonalves <saymonpadilhaalves10@gmail.com>
2026-03-27 11:35:28 -0300

    desafio 4

---
+++ atividades/exercicio4.js
@@ -0,0 +1,9 @@
+var produto1 = 1;
+var produto2  = 99;
+var valorTotal = produto1 + produto2;
+
+let mensagem = valorTotal > 100 ? " o total é de " + valorTotal : " com desconto ficou " + (valorTotal - (valorTotal * 0.10));
+console.log(mensagem); 
+
+
+
