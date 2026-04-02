ab4e330cadde2eb93591b7b1bc5f0fab72389d30
saymonalves <saymonpadilhaalves10@gmail.com>
2026-04-02 12:16:31 -0300

    exercicio 11

---
+++ atividades/exercicio11.js
@@ -0,0 +1,7 @@
+const nomes = ["saymon", "zoio", "kelven", "graff", "henrico", "galeto", "fanho", "alek", "felipão", "potrick",];
+if(nomes.includes["saymon"] = true){
+    console.log("nome localizado na lista");
+}
+else{
+    console.log("nome não localizado no sistema")
+}
