59c2183fdb77d008e400708dbb18aa97ae6e0e59
saymonalves <saymonpadilhaalves10@gmail.com>
2026-04-02 10:43:49 -0300

    exercicio 8

---
+++ atividades/exercicio8.js
@@ -0,0 +1,8 @@
+// crie uma funcao e receba doia parametros: o valor em Reais e a taxa de câmbiodo dia
+
+function conversor(valor, taxa){
+   return valor * taxa;
+}
+let valor = 10;
+let taxa = 5.50;
+console.log(conversor(valor, taxa));
